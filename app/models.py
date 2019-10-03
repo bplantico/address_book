ea79d8f06ec4715c9df026e3b5360a75f7694921
Brian Plantico <bplantico@gmail.com>
2019-10-03 14:06:36 -0600

    Adds migration for Address model along with SQLAlchemy and Migrate extensions to the __init__.py config file

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from app import db
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    address = db.Column(db.String(120))
+    city = db.Column(db.String(60))
+    state = db.Column(db.String(60))
+    zip = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Address {}, {}, {}, {}, {}, {}, {}>'.format(self.id, self.name, self.address, self.city, self.state, self.zip, self.timestamp)
