ed4c31c8aef166e164d48d84d350ea5f9f92afbc
Brian Plantico <bplantico@gmail.com>
2019-10-03 22:28:12 -0600

    Adds functionality to create an address with a POST request sent via a form at '/addresses/new'

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+class NewAddressForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    state = StringField('State', validators=[DataRequired()])
+    zip = IntegerField('Zip', validators=[DataRequired()])
+    submit = SubmitField('Create Address', validators=[DataRequired()])
